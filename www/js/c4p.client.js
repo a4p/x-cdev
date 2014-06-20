e3cf61fb2b44d7ec3b863ad1fe9beef39a2111b9
ja4p <jenkins@apps4pro.com>
2014-06-20 14:23:19 +0200

    j-dev-mobile-226

--- www/js/c4p.client.js
+++ www/js/c4p.client.js
@@ -1,4 +1,4 @@
-/*! c4p.client 2014-06-20 10:10 */
+/*! c4p.client 2014-06-20 14:07 */
 function rhex(num) {
     for (str = "", j = 0; 3 >= j; j++) str += hex_chr.charAt(num >> 8 * j + 4 & 15) + hex_chr.charAt(num >> 8 * j & 15);
     return str;
