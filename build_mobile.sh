a809203855f40d24345f7fb444de4def04a28d12
mlefree <mathieu.leprevost@apps4pro.com>
2014-06-30 10:01:48 +0200

    ds

--- build_mobile.sh
+++ build_mobile.sh
@@ -5,7 +5,7 @@ echo ""
 echo "[--- C4P : DEV Build mobile ---]"
 echo ""
 
-#sudo npm update -g cordova
+sudo npm update -g cordova
 rm -rf build
 
 cordova create build com.apps4pro.c4pdev c4p
@@ -19,8 +19,8 @@ sed -i.bak "s#$oldstring#$newstring#g" build/config.xml
 
 
 cd build
-#cordova platform add ios
-cordova platform add android
+cordova platform add ios
+#cordova platform add android
 
 cordova plugin add org.apache.cordova.device
 cordova plugin add org.apache.cordova.network-information
@@ -53,7 +53,7 @@ cordova plugin add https://github.com/mhweiner/CordovaiOSKeyboardPlugin.git
 #cordova plugin add https://github.com/Initsogar/cordova-webintent.git
 cordova plugin add https://github.com/mlefree/WebIntent.git
 
-#cordova build ios
+cordova build ios
 
 # Sed AndroidManifest
 oldstring='<\/intent-filter>'
@@ -63,18 +63,18 @@ newstring='<\/intent-filter ><intent-filter><action android:name="android.intent
 #newstring='<\/intent-filter ><intent-filter><action android:name="android.intent.action.VIEW"><\/action><category android:name="android.intent.category.DEFAULT"><\/category><category android:name="android.intent.category.BROWSABLE"><\/category><data android:scheme="content" android:mimeType="*\/*"><\/data><data android:scheme="file" android:mimeType="*\/*"\/><data android:host="www.youtube.com" android:scheme="http"><\/data><\/intent-filter>'
 sed -i.bak "s#$oldstring#$newstring#g" platforms/android/AndroidManifest.xml
 
-cordova prepare android
+#cordova prepare android
 oldstring='Theme.Black.NoTitleBar"'
 newstring='Theme.Black.NoTitleBar" android:launchMode="singleTask" android:clearTaskOnLaunch="true"'
-sed -i.bak "s#$oldstring#$newstring#g" platforms/android/AndroidManifest.xml
-cordova compile android
+#sed -i.bak "s#$oldstring#$newstring#g" platforms/android/AndroidManifest.xml
+#cordova compile android
 
-cd platforms/android
-ant release
-jarsigner -keystore ../../../../../c4p/c4p_html_ang/mobile_res/android_key/apps4pro-key.keystore -storepass apps4pro -digestalg SHA1 -sigalg MD5withRSA bin/C_d-release-unsigned.apk mykey
-cp bin/C_d-release-unsigned.apk ../../C_d.apk
-zipalign -f 4 ../../C_d.apk ../../C_d-aligned.apk
-cd ../..
+#cd platforms/android
+#ant release
+#jarsigner -keystore ../../../../../c4p/c4p_html_ang/mobile_res/android_key/apps4pro-key.keystore -storepass apps4pro -digestalg SHA1 -sigalg MD5withRSA bin/C_d-release-unsigned.apk mykey
+#cp bin/C_d-release-unsigned.apk ../../C_d.apk
+#zipalign -f 4 ../../C_d.apk ../../C_d-aligned.apk
+#cd ../..
 
 
 cd ..
